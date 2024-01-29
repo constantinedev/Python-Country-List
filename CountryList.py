dd241082e7beab17f708eaae33bf52a4ff62b67b
Constantine Dev <constantinedev@protonmail.com>
2024-01-29 14:12:02 +0000

    Update CountryList.py

--- CountryList.py
+++ CountryList.py
@@ -7,15 +7,15 @@ async def CountryList():
     timezones = pytz.country_timezones.get(country.alpha_2)
     if timezones:
       for timezone in timezones:
-		    offset = DT.now(pytz.timezone(timezone)).strftime("%z")
-		    offset = f"{offset[:-2]}:{offset[-2:]}"
+        offset = DT.now(pytz.timezone(timezone)).strftime("%z")
+        offset = f"{offset[:-2]}:{offset[-2:]}"
         _data = {
-	        "code": country.alpha_2,
-	        "name": country.name,
-	        "offset": offset,
-	        "timezone": timezone,
-	      }
-	      CountryList.append(_data)
+	  "code": country.alpha_2,
+	  "name": country.name,
+	  "offset": offset,
+	  "timezone": timezone,
+	}
+	CountryList.append(_data)
       else:
         pass
     return CountryList
