8e9a08097aa66d8883c5638d8facc45c2580d767
Constantine Dev <constantinedev@protonmail.com>
2024-01-29 14:10:25 +0000

    Update CountryList.py

--- CountryList.py
+++ CountryList.py
@@ -2,20 +2,20 @@ import pycountry, pytz
 from datetime import datetime as DT , timezone as TZ, timedelta as TD
 
 async def CountryList():
-	CountryList = []
-	for country in pycountry.countries:
-		timezones = pytz.country_timezones.get(country.alpha_2)
-		if timezones:
-			for timezone in timezones:
+  CountryList = []
+  for country in pycountry.countries:
+    timezones = pytz.country_timezones.get(country.alpha_2)
+    if timezones:
+      for timezone in timezones:
 				offset = DT.now(pytz.timezone(timezone)).strftime("%z")
 				offset = f"{offset[:-2]}:{offset[-2:]}"
-				_data = {
-					"code": country.alpha_2,
-					"name": country.name,
-					"offset": offset,
-					"timezone": timezone,
-				}
-				CountryList.append(_data)
-			else:
-				pass
-			return CountryList
+        _data = {
+	        "code": country.alpha_2,
+	        "name": country.name,
+	        "offset": offset,
+	        "timezone": timezone,
+	      }
+	      CountryList.append(_data)
+      else:
+        pass
+    return CountryList
